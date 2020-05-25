7aa6b98b726c01082ebc1723bbc2066b96b7f536
charlerae <contactcharlerae@gmail.com>
2020-05-25 01:01:11 -0400

    adds todoList object to index.js
    -todos array to hold todo list items
    -methods
      -showItems
      -addItem
      -changeItem
      -deleteItem

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 
 
-// It should store the todos array on an object
 
-// It should have a display todos method
+const todoList = {
+    // It should store the todos array on an object
+    todos: ['item 1', 'item 2', 'item 3', 'item 4', 'item 5'],
 
-// It should an add todo method
+    // It should an add todo method
+    addItem(newItem) {
+        this.todos.push(newItem);
+    },
 
-// It should have a change todo method
+    // It should have a display todos method
+    showItems() {
+        console.log(this.todos);
+    },
 
-// It should have a delete todo method
+    // It should have a change todo method
+    changeItem(index, newItem) {
+        this.todos[index] = newItem;
+    },
 
+    // It should have a delete todo method
+    deleteItem(index) {
+        this.todos.splice(index, 1);
+    },
+
+
+};
+
+console.log(todoList.todos);
+todoList.addItem('hello there')
+todoList.showItems();
+todoList.changeItem(5, 'BOOM');
+todoList.showItems();
+todoList.deleteItem(2);
+todoList.showItems();
+
+
+
+
+
+
+
+// Items below to be added in version 2
 // It should have an updating timestamp at the bottom
