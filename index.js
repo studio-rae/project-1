4abab9989f2e7b79304fe0a7c017059b74b6efda
charlerae <contactcharlerae@gmail.com>
2020-05-25 00:34:16 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// It should store the todos array on an object
+
+// It should have a display todos method
+
+// It should an add todo method
+
+// It should have a change todo method
+
+// It should have a delete todo method
+
+// It should have an updating timestamp at the bottom
